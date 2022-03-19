8d824dfe9411c359da65f0ae40f5944051bcd228
doobyeol <doobyeol@gmail.com>
2022-03-19 22:09:03 +0900

    모달 컴포넌트 등록

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -6,14 +6,24 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="fa-solid fa-plus addBtn"></i>
     </span>
+    <!-- use the modal component, pass in the prop -->
+    <Modal v-if="showModal" @close="showModal = false">
+      <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+      <h3 slot="header">custom header</h3>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
 export default {
   data: function () {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
   methods: {
@@ -24,11 +34,16 @@ export default {
         // this.$emit('이벤트 이름', 인자1, 인자2, ...);
         this.$emit("addTodoItem", this.newTodoItem);
         this.cliearInput();
+      } else {
+        // alert("왜 아무것도 안쳐");
       }
     },
     cliearInput: function () {
       this.newTodoItem = "";
     },
+  },
+  components: {
+    Modal: Modal,
   },
 };
 </script>
